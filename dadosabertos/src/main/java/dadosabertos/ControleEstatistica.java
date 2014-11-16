13a37542fdd2400b6af167a22a742150ebd31a5b
Naldo Cabral <naldo_rn@hotmail.com>
2014-11-16 13:38:53 -0300

    Alterei a classe ControleEstatistica para receber um parametro com valor do crime em um único metodo ao invés de criar um metodo para cada crime

--- dadosabertos/src/main/java/dadosabertos/ControleEstatistica.java
+++ dadosabertos/src/main/java/dadosabertos/ControleEstatistica.java
@@ -1,6 +1,7 @@
 package dadosabertos;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,41 +10,13 @@ public class ControleEstatistica {
 	
 	private ManipuladorXLSX planilhas;
 	
-	@RequestMapping("/homicidio")
-	public EstatisticasCriminais getHomicidio(){
-		planilhas = new ManipuladorXLSX("homicidio");
+	@RequestMapping("/")
+	public EstatisticasCriminais getHomicidio(@RequestParam(value="crime")String crime){
+			
+		planilhas = new ManipuladorXLSX(crime);
 		return planilhas.getEstatisticasCriminais();
+		
 	}
 	
-	@RequestMapping("/estupro")
-	public EstatisticasCriminais getEstupro(){
-		planilhas = new ManipuladorXLSX("estupro");
-		return planilhas.getEstatisticasCriminais();
-	}
 	
-	@RequestMapping("/rouboveiculo")
-	public EstatisticasCriminais getRouboVeiculo(){
-		planilhas = new ManipuladorXLSX("rouboveiculo");
-		return planilhas.getEstatisticasCriminais();
-	}
-	
-	@RequestMapping("/lesaocorporalseguidodemorte")
-	public EstatisticasCriminais getLesaoCorporal(){
-		planilhas = new ManipuladorXLSX("lesaocorporalseguidodemorte");
-		return planilhas.getEstatisticasCriminais();
-	}
-	
-	@RequestMapping("/latrocinio")
-	public EstatisticasCriminais getLatrocinio(){
-		planilhas = new ManipuladorXLSX("latrocinio");
-		return planilhas.getEstatisticasCriminais();
-	}
-	
-	@RequestMapping("/furtodeveiculo")
-	public EstatisticasCriminais getFurtoVeiculo(){
-		planilhas = new ManipuladorXLSX("furtodeveiculo");
-		return planilhas.getEstatisticasCriminais();
-	}
-	
-
 }
